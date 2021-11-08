9dc0b3f99d66be54b1d14d0e2aed69fea01aafa4
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2021-11-08 14:31:25 +0530

    Made block object and patterns of block

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,8 @@
 import pygame
 from pygame.color import THECOLORS
+from random import randint
+
+from src.objects import BLOCK
 
 def Write(text, x, y, color, size, screen, center=True):
     font = pygame.font.SysFont("Comis sans MS", size)
@@ -24,3 +27,37 @@ def transition(initial, final, time):
         yield current
     
     yield final
+
+def make_pattern(count : int, size:int) -> list:
+    """ This function will tell the position of different blocks in the block group """
+
+    patterns = [(200,-80)]
+    for i in range(count-1):
+        block = patterns[-1]
+
+        while block in patterns:
+            curr = randint(1, 4)
+            if curr == 1:
+                block = (block[0], block[1] -size)
+            elif curr == 2:
+                block = (block[0] + size, block[1])
+            elif curr == 3:
+                block == (block[0], block[1] + size)
+            elif curr == 4:
+                block = (block[0] - size, block[1])
+        
+        patterns.append(block)
+
+    return patterns
+
+def random_color():
+    """ This function will return a random color """
+    return (randint(0, 255), randint(0, 255), randint(0, 255))
+
+def make_block(pos, color) -> BLOCK:
+    block = BLOCK.copy()
+    block['x'] = pos[0]
+    block['y'] = pos[1]
+    block['color'] = color
+
+    return block
--- src/states/playstate.py
+++ src/states/playstate.py
@@ -1,6 +1,7 @@
+from random import randint
 import pygame
 from pygame.color import THECOLORS
-from src.functions import Write, transition
+from src.functions import Write, make_block, make_pattern, random_color, transition
 from src.states.base import Base
 
 class Play(Base):
@@ -8,26 +9,54 @@ class Play(Base):
     def __init__(self) -> None:
         super().__init__()
 
+        # if the game is started
         self.just_started = True
+
+        # current level
+        self.level = 1
+
+        # score
+        self.score = 0
+
+        # current pattern
+        self.current_pattern = make_pattern(randint(1, 5), 40)
+        self.moving_blocks = []
+        self.current_block_color = random_color()
+
+        for pattern in self.current_pattern:
+            block = make_block(pattern, self.current_block_color)
+            self.moving_blocks.append(block)
+
+        
+        # all blocks
+        self.all_blocks = []
 
     def render(self) -> None:
         
-        if not self.just_started : Write("Level 1", self.levelDisplayer_rect.width // 2, self.levelDisplayer_rect.height // 2, (255, 255, 255), 48,  self.levelDisplayer)
-        self.screen.blit(self.levelDisplayer, self.levelDisplayer_rect)
+        if self.just_started :
+            Write("Level 1", self.levelDisplayer_rect.width // 2, self.levelDisplayer_rect.height // 2, (255, 255, 255), 48,  self.levelDisplayer)
+            self.screen.blit(self.levelDisplayer, self.levelDisplayer_rect)
+
+        else:
+            for pattern in self.current_pattern:
+                print(pattern)
+                pygame.draw.rect(self.screen, pattern['color'], (pattern['x'], pattern['y'], pattern['width'], pattern['height']), 5)
 
 
     def update(self, params) -> None:
 
+        # game started animation
         if self.just_started:
             try:
                 self.levelDisplayer_rect.y = next(self.cordy)
-                if (self.levelDisplayer_rect.y == self.screen_width // 2 - 50):
-                    pygame.time.wait(2)
             except StopIteration:
-                self.just_started = False
+                if self.levelDisplayer_rect.y == self.screen_height // 2- 50:
+                    pygame.time.delay(1000)
+                    self.cordy = transition(self.levelDisplayer_rect.y, self.screen_height + 50, 30)
+                else:
+                    self.just_started = False
 
-
-        self.render()
+        self.render() 
 
     def enter(self, **param) -> None:
         self.screen = param['screen']
@@ -41,4 +70,4 @@ class Play(Base):
         self.levelDisplayer_rect.x = 0
         self.levelDisplayer_rect.y = -100
         
-        self.cordy = transition(-100, self.screen_height + 50, 120)
+        self.cordy = transition(-100, self.screen_height // 2- 50, 30)
